85ff92e9da35c2e3176cf75e154943f738736d1e
Usuario <wedipi5226@gmail.com>
2025-03-19 11:21:07 -0500

    Tailwind installed

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro"
+      }
+    }
+  ]
+};
